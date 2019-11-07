d8edf5a2efae75fdd1e2587dca7b8e02c515f99c
sullivanperret <54944889+sullivanperret@users.noreply.github.com>
2019-11-07 13:59:39 +0100

    test

---
+++ gsb/src/modele/dao/MedicamentDao.java
@@ -0,0 +1,12 @@
+package modele.dao;
+
+import modele.Medicament;
+
+public class MedicamentDao {
+
+	public static Medicament rechercher(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
